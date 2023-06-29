f68d0507364cc53c2930a6f991d422d1f25a02fd
Nick Young <nick.young@auckland.ac.nz>
2023-06-30 09:34:34 +1200

    tz handling, nicer output, safety sleep

--- send_emails.py
+++ send_emails.py
@@ -6,25 +6,37 @@ from jinja2 import Environment, FileSystemLoader
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from datetime import datetime, timedelta
-
-last_week = datetime.now() - timedelta(weeks=1)
-print(last_week)
+import time
 
 environment = Environment(loader=FileSystemLoader(""))
 template = environment.get_template("reminder_template.txt")
 
 df = pd.read_csv("responses.csv.zip")
-print(df)
-print(df["Q4"].value_counts())
 
-df.EndDate = pd.to_datetime(df.EndDate, errors="coerce")
+df.EndDate = pd.to_datetime(df.EndDate, errors="coerce", utc=True).dt.tz_convert("Pacific/Auckland")
+df["EndDate+1week"] = df.EndDate + pd.Timedelta(weeks=1)
 
-already_sent = pd.read_csv("sent_emails.csv")
+try:
+    already_sent = pd.read_csv("sent_emails.csv")
+except FileNotFoundError:
+    already_sent = pd.DataFrame(columns=["email", "sent_at"])
 
-# Filter to users that consented to email reminders, and haven't touched the survey in 7 days
-df = df[(df["Q4"] == "1") & (df["EndDate"] < last_week) & (~df["Q11_4"].isin(already_sent["email"]))]
-print(df)
+df.drop_duplicates(subset="Q11_4", inplace=True)
+df.dropna(subset=["Q11_4"], inplace=True)
+
+# Filter to users that consented to email reminders
+df = df[df["Q4"] == "1"]
+
+print(df[["StartDate", "EndDate", "EndDate+1week", "UN", "First", "Last", "Q11_4", "Q4"]].sort_values(by="EndDate"))
+
+# Filter to users that haven't touched the survey in 1 week and haven't already had a reminder
+df = df[(df["EndDate+1week"] < pd.Timestamp.now(tz="Pacific/Auckland")) & (~df["Q11_4"].isin(already_sent["email"]))]
+if len(df) == 0:
+    print("No emails need to be sent")
+    exit(1)
+
+print(f"Will start sending {len(df)} emails in 10 seconds, CTRL-C now or forever hold your peace")
+time.sleep(10)
 
 mailserver = smtplib.SMTP("mailhost.auckland.ac.nz")
 
